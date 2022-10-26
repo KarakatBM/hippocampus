2e104b7fef6465a873de238c4c56de6cd6e39c6e
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-10-26 12:31:52 +0600

    initial commit

---
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        getSupportActionBar()?.setTitle("Home")
+    }
+}
