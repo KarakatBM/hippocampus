4ff32de38024c1a4d4781c972b9f6fcb68069463
Nursaya03 <rlb_03@mail.ru>
2022-11-02 01:33:30 +0600

    HomePageFragment.kt changed + added to navigation graph

--- app/src/main/java/com/example/finalproject/HomePageFragment.kt
+++ app/src/main/java/com/example/finalproject/HomePageFragment.kt
@@ -7,11 +7,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.finalproject.databinding.FragmentHomePageBinding
 
 class HomePageFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false)
+        val binding: FragmentHomePageBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home_page, container, false)
+
+        // Add OnClick Handler for CardView
+        binding.cardFlashcards.setOnClickListener{ view: View->
+            view.findNavController()
+                .navigate(R.id.action_homePageFragment_to_foldersPageFragment) }
+
+    return binding.root
     }
 }
