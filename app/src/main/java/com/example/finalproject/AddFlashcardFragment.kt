3b33600f0c5c95074e0268690ed9660293e92a4f
Nursaya03 <rlb_03@mail.ru>
2022-11-02 02:32:15 +0600

    AddFlashcardFragment.kt & MemorizationFragment.kt changed

--- app/src/main/java/com/example/finalproject/AddFlashcardFragment.kt
+++ app/src/main/java/com/example/finalproject/AddFlashcardFragment.kt
@@ -7,11 +7,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.finalproject.databinding.FragmentFlashcardPageBinding
+
 
 class AddFlashcardFragment : Fragment() {
+    private var binding: FragmentFlashcardPageBinding? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_flashcard, container, false)
+        binding = FragmentFlashcardPageBinding.inflate(inflater, container, false)
+
+//      TODO: Add OnClick Handler for Add Card button
+
+        return binding!!.root
     }
 }
