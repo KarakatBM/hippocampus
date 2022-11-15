09ced7cff42697a068b1f63f9dca4f6d1db96cb3
Karakat <200107021@stu.sdu.edu.kz>
2022-11-15 16:46:24 +0600

    Revert "fragmnet_nav_drawer"
    This reverts commit 4209ad78ade74c6456e0c9dde7afcadc89892e6e.

--- app/src/main/java/com/example/finalproject/HomePageFragment.kt
+++ app/src/main/java/com/example/finalproject/HomePageFragment.kt
@@ -8,31 +8,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.navigation.findNavController
-import com.example.finalproject.databinding.ActivityMainBinding
 import com.example.finalproject.databinding.FragmentHomePageBinding
-import com.example.finalproject.databinding.FragmentToolbarBinding
 
 class HomePageFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+                              savedInstanceState: Bundle?): View? {
 
         // Inflate the layout for this fragment
-        val binding: ViewDataBinding? = DataBindingUtil.inflate(
+        val binding: FragmentHomePageBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_home_page, container, false)
 
         // Add OnClick Handler for CardViews
-        binding?.root?.setOnClickListener{ view: View->
+        binding.cardFlashcards.setOnClickListener{ view: View->
             view.findNavController()
                 .navigate(R.id.action_homePageFragment_to_foldersPageFragment) }
 
         //   TODO: Implement other handlers
 
-        binding?.root?.setOnClickListener{ view: View->
+        binding.notes.setOnClickListener{ view: View->
             view.findNavController()
                 .navigate(R.id.action_homePageFragment_to_notesFragment) }
 
-            return binding!!.root
+        return binding.root
     }
 }
