f3cf294a624a3ad65ba7303f3d843be8a5ced080
Nursaya03 <rlb_03@mail.ru>
2022-12-09 01:27:53 +0600

    NotesViewModel.kt + fragment_note_item.xml added

---
+++ app/src/main/java/com/example/finalproject/viewmodel/NotesViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.finalproject.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.finalproject.database.Notes
+import com.example.finalproject.database.NotesDatabase
+import com.example.finalproject.database.NotesRepository
+import kotlinx.coroutines.launch
+
+class NotesViewModel (application: Application): AndroidViewModel(application) {
+
+	val allNotes : LiveData<List<Notes>>
+	val repository : NotesRepository
+
+	init {
+		val dao = NotesDatabase.getDatabase(application).getNotesDao()
+		repository = NotesRepository(dao)
+		allNotes = repository.allNotes
+	}
+
+	fun deleteNote (note: Notes) = viewModelScope.launch {
+		repository.delete(note)
+	}
+
+	fun updateNote(note: Notes) = viewModelScope.launch {
+	repository.update(note)
+	}
+
+	fun addNote(note: Notes) = viewModelScope.launch{
+		repository.insert(note)
+	}
+}
