4f3055796b5fdc8c34d019abf6226595fbe846a3
Karakat <200107021@stu.sdu.edu.kz>
2022-12-09 10:45:57 +0600

    internet

---
+++ app/src/main/java/com/example/finalproject/detail/DetailViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.finalproject.detail
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.finalproject.network.MarsProperty
+
+
+class DetailViewModel(@Suppress("UNUSED_PARAMETER")marsProperty: MarsProperty, app: Application) : AndroidViewModel(app) {
+}
---
+++ app/src/main/java/com/example/finalproject/overview/OverviewFragment.kt
@@ -0,0 +1,37 @@
+package com.example.finalproject.overview
+
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.finalproject.R
+import com.example.finalproject.databinding.FragmentOverviewBinding
+
+class OverviewFragment : Fragment() {
+
+    private val viewModel: OverviewViewModel by lazy {
+        ViewModelProvider(this).get(OverviewViewModel::class.java)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = FragmentOverviewBinding.inflate(inflater)
+
+        binding.lifecycleOwner = this
+
+
+        binding.viewModel = viewModel
+
+      //  binding.photosGrid.adapter = PhotoGridAdapter()
+
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.navdrawer_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+}
