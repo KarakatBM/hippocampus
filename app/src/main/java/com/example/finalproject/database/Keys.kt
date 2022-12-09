cc6e9db33f5ebe5d87d91bca7202f2433bd58218
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-12-09 11:01:20 +0600

    pomodoro database fixed

---
+++ app/src/main/java/com/example/finalproject/database/Keys.kt
@@ -0,0 +1,9 @@
+package com.example.finalproject.database
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "keys_table")
+data class Keys(
+       @PrimaryKey(autoGenerate = true)
+       var keyId: Long = 0L
+)
